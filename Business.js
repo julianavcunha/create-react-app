3e1c4b09c5e17c8524960540d0c65fa9ec5b50a7
julianavcunha <84291340+julianavcunha@users.noreply.github.com>
2026-06-04 15:51:21 -0300

    Add react-bootstrap import to Business component

--- Business.js
+++ Business.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Business from './fakeBiz.js'
+import * as reactBootstrap from 'https://esm.run/react-bootstrap';
 
 function compBusiness() {
     return <Business />;
